9d8157ff1a11ac6b7c155a9b659fc9797cfda948
Thaiscampo <thais.campo123@gmail.com>
2021-11-12 22:06:05 -0300

    Deploy o Heroku

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,7 +1,7 @@
 import axios from 'axios'
  
 const api = axios.create({
-    baseURL: 'https://backendtarefa0.herokuapp.com/'
+    baseURL: 'https://backendtarefa0.herokuapp.com/tasks'
 })
  
 export default api;
